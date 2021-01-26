c63f81c4fbd080c2f930599247748aed5c111426
DanilMazurkin <danilmazurkin27@gmail.com>
2021-01-26 15:26:20 +1000

    Fix

--- request.py
+++ request.py
@@ -1,24 +1,28 @@
 import requests
+import json
 
 url_get = 'http://httpbin.org/'
 url_post = 'http://httpbin.org/post'
 url_json = 'https://httpbin.org/json'
 url_image = 'https://httpbin.org/image/jpeg'
 
+HEADERS_JSON = {
+    'Content-type': 'application/json', 
+    'Accept': 'text/plain'
+}
+
+
 response_get = requests.get(url_get)
 response_post = requests.post(url_post)
-response_json = requests.get(url_json)
+response_json = requests.get(url_json, headers=HEADERS_JSON)
 response_image = requests.get(url_image)
 
 
 page_content = response_get.text
 post_content = response_post.json()
 
+with open("sample_image.png", "wb") as file_image:
+    file_image.write(response_image.content)
 
-file_image = open("sample_image.png", "wb")
-file_image.write(response_image.content)
-file_image.close()
-
-file_json = open('file.json', "wb")
-file_json.write(response_json.content)
-file_json.close()
+with open('file.json', "w") as file_json:
+    json.dump(response_json.json(), file_json, indent=5)
