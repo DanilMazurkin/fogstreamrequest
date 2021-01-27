f0ee514f2ee95f37fdf8546d108e6deeff553255
DanilMazurkin <danilmazurkin27@gmail.com>
2021-01-27 18:20:57 +1000

    Add headers

--- request.py
+++ request.py
@@ -6,11 +6,60 @@ url_post = 'http://httpbin.org/post'
 url_json = 'https://httpbin.org/json'
 url_image = 'https://httpbin.org/image/jpeg'
 
+HEADER_GET = {
+    'Host': 'httpbin.org',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0)' 
+                  'Gecko/20100101 Firefox/84.0',
+    'Accept': 'text/html,application/xhtml+xml,application/'
+              'xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+}
 
-response_get = requests.get(url_get)
-response_post = requests.post(url_post)
-response_json = requests.get(url_json)
-response_image = requests.get(url_image)
+HEADER_POST = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Content-Length": "0",
+    "Host": "httpbin.org",
+    "Origin": "moz-extension://e01df379-8eab-4dba-bdf0-0403b3376a20",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
+    "X-Amzn-Trace-Id": "Root=1-6011204f-64465e20539b0bc55227be6a"
+}
+
+HEADER_JSON = {
+    'Host': 'httpbin.org',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0)' 
+                  'Gecko/20100101 Firefox/84.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;'
+               'q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+}
+
+HEADER_IMAGE = {
+    'Host': 'httpbin.org',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0)' 
+                  'Gecko/20100101 Firefox/84.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;'
+              'q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+}
+
+response_get = requests.get(url_get, headers=HEADER_GET)
+response_post = requests.post(url_post, headers=HEADER_POST)
+response_json = requests.get(url_json, headers=HEADER_JSON)
+response_image = requests.get(url_image, headers=HEADER_IMAGE)
 
 
 page_content = response_get.text
